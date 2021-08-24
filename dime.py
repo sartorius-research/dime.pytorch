ed6744db6c99bd15f1681046173723ba04d699c1
rickard.sjoegren <rickard.sjoegren@sartorius.com>
2021-08-24 10:34:43 +0200

    Updated docstring

--- dime.py
+++ dime.py
@@ -39,30 +39,31 @@ class DIME:
     Examples
     --------
     Given a 2D-tensor, fit the hyperplane.
-    >>> x  # N x C torch 2D float-tensor.
+
+    >>> x = torch.tensor(...) # N x P torch 2D float-tensor.
     >>> modelled_embedding = DIME().fit(x)
 
     To obtain probabilities, calibrate percentiles. Preferably against
-    separate dataset. Chaining is fine.
-    >>> x  # N x P torch 2D float-tensor.
-    >>> x_cal  # N x P torch 2D float-tensor.
+    separate dataset. Chaining is fine.:
+
+    >>> x_cal = torch.tensor(...)  # N_cal x P torch 2D float-tensor.
     >>> modelled_embedding = DIME().fit(x).calibrate(x_cal)
 
-    Given fitted hyperplane, you can calculate distances on new observations
-    >>> x_new  # N x P torch 2D float-tensor.
-    >>> modelled_embedding.distance_to_hyperplane(x_new)
+    Given fitted hyperplane, you can calculate distances on new observations:
 
-    Or within hyperplane.
-    >>> x_new  # N x P torch 2D float-tensor.
-    >>> modelled_embedding.distance_within_hyperplane(x_new)  # -> 1D float-tensor. 
+    >>> x_new = torch.tensor(...)  # N_new x P 2D float-tensor.
+    >>> modelled_embedding.distance_to_hyperplane(x_new)  # -> 1D float-tensor, length N_new
 
     To obtain probabilities of that the new observations have a distance
-    calibration set observations are equal or less than the new distance, 
+    calibration set observations are equal or less than the new distance,
     you need to have calibrated the percentiles as shown above. Then you
     receive the probablities by passing `return_probablities`-keyword:
-    
-    >>> x_new  # N x P torch 2D float-tensor.
-    >>> modelled_embedding.distance_within_hyperplane(x_new, return_probabilites=True) # -> 1D float-tensor. 
+
+    >>> modelled_embedding.distance_to_hyperplane(x_new, return_probabilites=True) # -> 1D float-tensor, length N_new
+
+    You can also use the alternative formulation of distance within the hyperplane, optionally as probabilities:
+
+    >>> modelled_embedding.distance_within_hyperplane(x_new)  # -> 1D float-tensor, length N_new
     """
     
     def __init__(self, r2_threshold=0.99):
