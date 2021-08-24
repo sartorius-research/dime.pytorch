70524fb8cd51e7b673a91e0a64a0272c21d59e5b
rickard.sjoegren <rickard.sjoegren@sartorius.com>
2021-08-24 12:30:36 +0200

    Added tests

---
+++ test_dime.py
@@ -0,0 +1,19 @@
+import pytest
+import torch
+
+from dime import DIME
+
+
+def test_creation():
+    # Should work.
+    DIME()
+    DIME(explained_variance_threshold=10)
+    DIME(explained_variance_threshold=0.9)
+    DIME(n_percentiles=10)
+
+
+def test_raises_on_bad_arguments():
+    with pytest.raises(ValueError):
+        DIME(explained_variance_threshold=1.1)
+    with pytest.raises(ValueError):
+        DIME(n_percentiles=-1)
