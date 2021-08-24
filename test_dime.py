566bd5b4aa44aeb885a01d1c033ee30ea41b3ae3
rickard.sjoegren <rickard.sjoegren@sartorius.com>
2021-08-24 12:47:04 +0200

    More tests

--- test_dime.py
+++ test_dime.py
@@ -21,3 +21,47 @@ def test_raises_on_bad_arguments():
         DIME(explained_variance_threshold=-.1)
     with pytest.raises(ValueError):
         DIME(n_percentiles=-1)
+
+
+def test_fit_embedding():
+    p = 30
+    x = torch.randn(2000, p)
+    modelled_embedding = DIME().fit(x)
+
+    assert modelled_embedding.hyperplane_basis_vectors.shape[0] == p
+
+
+def test_fit_embedding_with_specific_rank():
+    p = 30
+    rank = 5
+    x = torch.randn(2000, p)
+    modelled_embedding = DIME(explained_variance_threshold=5).fit(x)
+
+    assert modelled_embedding.hyperplane_basis_vectors.shape[1] == rank
+
+
+def test_fit_embedding_with_specific_ratio():
+    p = 30
+    ratio = 0.5
+    x = torch.randn(2000, p)
+    modelled_embedding = DIME(explained_variance_threshold=ratio).fit(x)
+
+    assert modelled_embedding.explained_variance <= ratio
+
+
+def test_get_distance_to_hyperplane():
+    n = 2000
+    x = torch.randn(n, 30)
+    modelled_embedding = DIME().fit(x)
+
+    dime = modelled_embedding.distance_to_hyperplane(x)
+    assert dime.shape == (2000, )
+
+
+def test_get_distance_within_hyperplane():
+    n = 2000
+    x = torch.randn(n, 30)
+    modelled_embedding = DIME().fit(x)
+
+    dime = modelled_embedding.distance_within_hyperplane(x)
+    assert dime.shape == (2000, )
